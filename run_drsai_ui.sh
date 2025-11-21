46acece9620c4bc9348dcc6e31d7e1f9904839c1
xdb@mail.ustc.edu.cn <xdb@mail.ustc.edu.cn>
2025-11-21 22:50:54 +0800

    add a start shell

---
+++ run_drsai_ui.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 检查根目录下是否存在.env文件
+if [ ! -f ".env" ]; then
+    echo "错误: .env文件不存在，请先创建.env文件"
+    exit 1
+fi
+
+# 检查frontend目录下是否存在.env.development文件
+if [ ! -f "./frontend/.env.development" ]; then
+    echo "错误: frontend/.env.development文件不存在，请先创建该文件"
+    exit 1
+fi
+
+pm2 start -n drsai_backend "drsai ui"
+
+cd ./frontend
+pm2 start -n drsai_frontend "yarn run dev "
+
